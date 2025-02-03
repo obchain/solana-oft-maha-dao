4e8a1327c1d0de22aa0ed4f544c8f611df3b04bc
obchain <riteishnikhoria@gmail.com>
2025-02-03 11:50:28 +0530

    added deployment + updated config

--- deploy/MyOFT.ts
+++ deploy/MyOFT.ts
@@ -2,7 +2,7 @@ import assert from 'assert'
 
 import { type DeployFunction } from 'hardhat-deploy/types'
 
-const contractName = 'MyOFT'
+const contractName = 'MyOFTMock'
 
 const deploy: DeployFunction = async (hre) => {
     const { getNamedAccounts, deployments } = hre
@@ -36,8 +36,8 @@ const deploy: DeployFunction = async (hre) => {
     const { address } = await deploy(contractName, {
         from: deployer,
         args: [
-            'MyOFT', // name
-            'MOFT', // symbol
+            'MAHA OFT Token', // name
+            'MAHAOFT', // symbol
             endpointV2Deployment.address, // LayerZero's EndpointV2 address
             deployer, // owner
         ],
